eaf758a8dba5f657bca3088e7ad58144cb19ba79
NaotoF <2016asap@gmail.com>
2022-05-28 10:03:36 +0900

    finish memo

---
+++ main.rb
@@ -0,0 +1,23 @@
+require "csv"
+ 
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+memo_type = gets.to_s
+input_number = memo_type.chomp
+
+if input_number == "1"
+puts "拡張子を除いたファイルを入力してください。"
+file_name = gets.chomp
+
+puts "メモしたい内容を記入してください。"
+puts "終了したらCtrl＋Dを押します"
+# 複数行を「文字列」として読み取り。入力がされ Ctrl+D または Ctrl+Z の後 エンターキーが押されるまで待機。readlinesは配列として読み取り。
+input_memo = STDIN.read
+memo = input_memo.chomp
+# "CSV.open('test.csv','w')"の'w'で書き込み。'a'は追記モード。
+CSV.open("#{file_name}.csv" , "w") do |csv|
+# test.puts ["A","B","C"]
+# test << ["A","B","C"]
+csv.puts ["#{memo}"]
+end
+
+end
