b15830ee672965130c14138d820651e18bafe642
NaotoF <2016asap@gmail.com>
2022-05-28 21:31:48 +0900

    finish memo app

--- main.rb
+++ main.rb
@@ -20,4 +20,21 @@ CSV.open("#{file_name}.csv" , "w") do |csv|
 csv.puts ["#{memo}"]
 end
 
+elsif input_number == "2"
+  puts "編集するファイル名を拡張子を除いて入力してください。"
+  file_name = gets.chomp
+
+  puts "追記する内容を入力してください。"
+  puts "終了したらCtrl＋Dを押します"
+  input_edition = STDIN.read
+
+  # 複数行を「文字列」として読み取り。入力がされ Ctrl+D または Ctrl+Z の後 エンターキーが押されるまで待機。readlinesは配列として読み取り。
+  editted_memo = input_edition.chomp
+  # "CSV.open('test.csv','w')"の'w'で書き込み。'a'は追記モード。
+  CSV.open("#{file_name}.csv" , "a") do |csv|
+  # test.puts ["A","B","C"]
+  # test << ["A","B","C"]
+  csv.puts ["#{editted_memo}"]
+  end
 end
+
